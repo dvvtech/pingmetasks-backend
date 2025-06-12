46cf013543351ce107b299cee203d876be6f3076
DVV_COMPUTER\biker <dvv153m@gmail.com>
2025-06-12 08:29:50 +0300

    add swagger

--- PingMeTasks/src/PingMeTasks.Api/Program.cs
+++ PingMeTasks/src/PingMeTasks.Api/Program.cs
@@ -3,8 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 
